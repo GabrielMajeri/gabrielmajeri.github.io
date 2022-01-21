f3d57d8a74fc829692ad45c98a97b1bd693bc3f6
Gabriel Majeri <gabriel.majeri6@gmail.com>
2022-01-21 10:13:53 +0200

    Try to fix up Gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
 module.exports = {
+  siteMetadata: {
+    title: `Gabriel Majeri's personal website`,
+    description: `My personal web page on the internet.`,
+    author: `@GabrielMajeri`,
+    siteUrl: `https://www.gabrielmajeri.ro/`,
+  },
   plugins: [`gatsby-plugin-react-helmet`],
 };
