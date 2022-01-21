d48c3eb2816c11fd0a74acf79a8ed8a62256850d
Gabriel Majeri <gabriel.majeri6@gmail.com>
2022-01-21 09:04:12 +0200

    Implement simple component unit tests

---
+++ src/pages/index.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import HomePage from "./index";
+
+test("Displays my name", () => {
+  const { queryByText } = render(<HomePage />);
+
+  expect(queryByText("Gabriel Majeri")).toBeTruthy();
+});
