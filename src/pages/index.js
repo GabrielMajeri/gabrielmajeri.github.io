379423e95bc54345181936b709694138f700b733
Gabriel Majeri <gabriel.majeri6@gmail.com>
2023-04-30 11:10:39 +0300

    Update dependencies and uncomment code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,107 +1,110 @@
 import React from "react";
-// import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet";
 
-const HomePage = () => <p></p>;
+const HomePage = () => (
+  <main>
+    <Helmet>
+      <html lang="en" />
+
+      <meta charSet="utf-8" />
+      <meta name="author" content="Gabriel Majeri" />
+
+      <title>Gabriel Majeri &mdash; Welcome!</title>
+
+      {/*
+        Open all links in a new tab/window by default, and prevent
+        the target page from accessing this page's object.
+      */}
+      <base target="_blank" rel="noopener" />
+    </Helmet>
+    <header>
+      <h1>Gabriel Majeri</h1>
+      <p>Student & open-source contributor</p>
+    </header>
+    <section>
+      <h2>Projects</h2>
+      <p>
+        I keep most of my projects{" "}
+        <a href="https://github.com/GabrielMajeri">on GitHub</a>. I strongly
+        believe freely sharing knowledge on the internet is essential for a
+        healthy society.
+      </p>
+      <p>
+        I also have a <a href="https://blog.gabrielmajeri.ro/">blog</a>,
+        although I don't write in it as often as I'd want.
+      </p>
+      <p>
+        Check out my <a href="https://cv.gabrielmajeri.ro">CV</a> for more
+        details about what I've achieved.
+      </p>
+    </section>
+    {/*
+    <section>
+      <h2>Web Development Portfolio</h2>
+      <p>
+        The modern web has evolved to be the ultimate medium of expression. The
+        best way to get your story across is through a well-designed web site.
+      </p>
+      <p>
+        I initially <em>despised</em> web design and found it boring; it seemed
+        to provide no intellectual challenge. Over time, I've grown to realise
+        the importance of presenting my ideas in a concise and accessible
+        manner, and have since challenged myself to improve my web development
+        skills.
+      </p>
+      <p>
+        I've worked on the projects below as part of the{" "}
+        <a href="https://www.freecodecamp.org/">freeCodeCamp</a> certification
+        &mdash; in my opinion, still the best resource for learning web
+        development.
+      </p>
+      <h3>Front End Projects</h3>
+      <ul>
+        <li>
+          <a href="https://www.gabrielmajeri.ro/fcc-tribute-page/">
+            Tribute Page
+          </a>
+        </li>
+        <li>
+          <a href="https://www.gabrielmajeri.ro/fcc-survey-form/">
+            Survey Form
+          </a>
+        </li>
+        <li>
+          <a href="https://www.gabrielmajeri.ro/fcc-product-landing-page/">
+            Product Landing Page
+          </a>
+        </li>
+      </ul>
+      <h3>React Projects</h3>
+      <ul>
+        <li>
+          <a href="https://www.gabrielmajeri.ro/fcc-markdown-previewer/">
+            Markdown previewer
+          </a>
+        </li>
+      </ul>
+    </section>
+    */}
+    <section>
+      <h2>Contact</h2>
+      <p>
+        The best way to reach me is through my{" "}
+        <a href="mailto:gabriel.majeri6@gmail.com">e-mail</a>.
+      </p>
+      <p>
+        You can find me on{" "}
+        <a href="https://www.linkedin.com/in/gabriel-majeri/">LinkedIn</a>.
+        <br />
+        Feel free to drop a message, although be advised I tend not to accept
+        connections from people I haven't met beforehand.
+      </p>
+    </section>
+    <footer>
+      This site is hosted on{" "}
+      <a href="https://pages.github.com/">GitHub Pages</a>.
+    </footer>
+  </main>
+);
+
 export default HomePage;
-
-// const HomePage = () => (
-//   <main>
-//     <Helmet>
-//       <html lang="en" />
-
-//       <meta charSet="utf-8" />
-//       <meta name="author" content="Gabriel Majeri" />
-
-//       <title>Gabriel Majeri &mdash; Welcome!</title>
-
-//       {/*
-//         Open all links in a new tab/window by default, and prevent
-//         the target page from accessing this page's object.
-//       */}
-//       <base target="_blank" rel="noopener" />
-//     </Helmet>
-//     <header>
-//       <h1>Gabriel Majeri</h1>
-//       <p>Student & open-source contributor</p>
-//     </header>
-//     <section>
-//       <h2>Projects</h2>
-//       <p>
-//         I keep most of my projects{" "}
-//         <a href="https://github.com/GabrielMajeri">on GitHub</a>. I strongly
-//         believe freely sharing knowledge on the internet is essential for a
-//         healthy society.
-//       </p>
-//       <p>
-//         I also have a <a href="https://blog.gabrielmajeri.ro/">blog</a>,
-//         although I don't write in it as often as I'd want.
-//       </p>
-//     </section>
-//     <section>
-//       <h2>Web Development Portfolio</h2>
-//       <p>
-//         The modern web has evolved to be the ultimate medium of expression. The
-//         best way to get your story across is through a well-designed web site.
-//       </p>
-//       <p>
-//         I initially <em>despised</em> web design and found it boring; it seemed
-//         to provide no intellectual challenge. Over time, I've grown to realise
-//         the importance of presenting my ideas in a concise and accessible
-//         manner, and have since challenged myself to improve my web development
-//         skills.
-//       </p>
-//       <p>
-//         I've worked on the projects below as part of the{" "}
-//         <a href="https://www.freecodecamp.org/">freeCodeCamp</a> certification
-//         &mdash; in my opinion, still the best resource for learning web
-//         development.
-//       </p>
-//       <h3>Front End Projects</h3>
-//       <ul>
-//         <li>
-//           <a href="https://www.gabrielmajeri.ro/fcc-tribute-page/">
-//             Tribute Page
-//           </a>
-//         </li>
-//         <li>
-//           <a href="https://www.gabrielmajeri.ro/fcc-survey-form/">
-//             Survey Form
-//           </a>
-//         </li>
-//         <li>
-//           <a href="https://www.gabrielmajeri.ro/fcc-product-landing-page/">
-//             Product Landing Page
-//           </a>
-//         </li>
-//       </ul>
-//       <h3>React Projects</h3>
-//       <ul>
-//         <li>
-//           <a href="https://www.gabrielmajeri.ro/fcc-markdown-previewer/">
-//             Markdown previewer
-//           </a>
-//         </li>
-//       </ul>
-//     </section>
-//     <section>
-//       <h2>Contact</h2>
-//       <p>
-//         The best way to reach me is through my{" "}
-//         <a href="mailto:gabriel.majeri6@gmail.com">email</a>.
-//       </p>
-//       <p>
-//         You can find me on{" "}
-//         <a href="https://www.linkedin.com/in/gabriel-majeri/">LinkedIn</a>.
-//         <br />
-//         Feel free to drop a message, although be advised I don't accept
-//         connections from people I haven't met beforehand.
-//       </p>
-//     </section>
-//     <footer>
-//       This site is hosted for free on{" "}
-//       <a href="https://pages.github.com/">GitHub Pages.</a>
-//     </footer>
-//   </main>
-// );
-
-// export default HomePage;
