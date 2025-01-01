ec680f44542ad21586ad02d5806f454e5c6f20ad
Gabriel Majeri <gabriel.majeri6@gmail.com>
2025-01-01 14:08:38 +0200

    Switch from Gatsby to Astro

--- gatsby-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: `Gabriel Majeri's personal website`,
-    description: `My personal web page on the internet.`,
-    author: `@GabrielMajeri`,
-    siteUrl: `https://www.gabrielmajeri.ro/`,
-  },
-  plugins: [`gatsby-plugin-react-helmet`],
-};
